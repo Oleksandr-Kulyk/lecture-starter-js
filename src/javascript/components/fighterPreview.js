8753f3c2feb532ffa6e02bd304e7ee4314126de7
Miwka <miwka@pop-os.netis.cc>
2026-05-26 21:01:34 +0300

    + Created logic and styles for displaying fighter previev

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,16 +1,6 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
+const FIGHTER_STATS = ['health', 'attack', 'defense'];
 
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
@@ -26,4 +16,45 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+function createFighterName(name) {
+    const nameElement = createElement({ tagName: 'h3', className: 'fighter-preview___name' });
+    nameElement.textContent = name;
+
+    return nameElement;
+}
+
+function createFighterStat(statName, statValue) {
+    const statElement = createElement({ tagName: 'li', className: 'fighter-preview___stat' });
+    statElement.textContent = `${statName}: ${statValue}`;
+
+    return statElement;
+}
+
+function createFighterStats(fighter) {
+    const statsElement = createElement({ tagName: 'ul', className: 'fighter-preview___stats' });
+    const statElements = FIGHTER_STATS.map(stat => createFighterStat(stat, fighter[stat]));
+
+    statsElement.append(...statElements);
+
+    return statsElement;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+
+    if (fighter) {
+        fighterElement.append(
+            createFighterImage(fighter),
+            createFighterName(fighter.name),
+            createFighterStats(fighter)
+        );
+    }
+
+    return fighterElement;
 }
